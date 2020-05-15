22cfba0d59eba57e772bff251160927408b5ef3f
Amir <amirz98@hotmail.com>
2020-05-16 07:05:35 +0800

    Security: Separate credentials from main code

--- ESP8266_Main_Adalight.ino
+++ ESP8266_Main_Adalight.ino
@@ -1,15 +1,22 @@
+/*
+ * To setup credentials and other settings create a "settings.h" file in root of this sketch with this code below:
+  #define WIFI_SSID "My_Wi-Fi"
+  #define WIFI_PASSWORD "password"
+  
+  #define MQTT_HOST IPAddress(192, 168, 1, 10)
+  #define MQTT_PORT 1883
+  #define MQTT_USER "mqttUSR"
+  #define MQTT_PASS "mqttPASS"
+  #define OTA_PASS "otaPASS"
+ */
+
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
 #include <AsyncMqttClient.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-
-#define WIFI_SSID "My_Wi-Fi"
-#define WIFI_PASSWORD "my-awesome-password"
-
-#define MQTT_HOST IPAddress(192, 168, 1, 10)
-#define MQTT_PORT 1883
+#include "settings.h"
 
 AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
@@ -75,7 +82,7 @@ void otaSetup()
   // ArduinoOTA.setHostname("myesp8266");
 
   // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+  ArduinoOTA.setPassword(OTA_PASS);
 
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
